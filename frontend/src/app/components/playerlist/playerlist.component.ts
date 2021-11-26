033ab97a2803e64d0ec4a50e575e696f4abd3cb3
Lias <kleisa@puzzle.ch>
2021-11-26 16:45:04 +0100

    Start with component playerlist, it works and now in progress is calling all backend url's in the poker.service class. add also images for game

---
+++ frontend/src/app/components/playerlist/playerlist.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs";
+import {Player} from "../../player";
+import {PokerService} from "../../poker.service";
+
+@Component({
+  selector: 'app-playerlist',
+  templateUrl: './playerlist.component.html',
+  styleUrls: ['./playerlist.component.scss']
+})
+
+
+export class PlayerListComponent implements OnInit {
+
+     players: Player[] = [];
+     splitarray: string[] = [];
+     gamekey?: string;
+     playerid?: number;
+     playerMode?: Object;
+     average?: number;
+
+
+    constructor(private pokerservice: PokerService) { }
+
+    createTablemaster() {
+        this.pokerservice.createTablemaster("test", "testg").subscribe((text) => {this.gamekey = text});
+    }
+    createPlayer() {
+        this.splitarray = this.gamekey!.split(",");
+
+        this.pokerservice.createPlayer(this.splitarray[0], "name").subscribe((text) => {this.playerid = text});
+    }
+    setSelectedCard() {
+        this.splitarray = this.gamekey!.split(",");
+
+        this.pokerservice.setSelectedCard(this.splitarray[0], this.playerid!, "3");
+    }
+    setPlayerMode() {
+        this.splitarray = this.gamekey!.split(",");
+
+        this.pokerservice.setPlayerMode(this.splitarray[0], this.playerid!, false);
+    }
+    getPlayerMode() {
+        this.splitarray = this.gamekey!.split(",");
+
+        this.pokerservice.getPlayerMode(this.splitarray[0], this.playerid!).subscribe((text) => {this.playerMode = text});
+    }
+    getAvergae() {
+        this.splitarray = this.gamekey!.split(",");
+
+        this.pokerservice.getAverage(this.splitarray[0]).subscribe((text) => {this.average = text});
+    }
+
+
+    getplayerscall() {
+        this.splitarray = this.gamekey!.split(",");
+
+        this.pokerservice.getPlayers(this.splitarray[0]).subscribe((text) => {this.players = text});
+    }
+
+    ngOnInit(): void {
+    }
+}
---
+++ frontend/src/app/poker.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Player} from "./player";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokerService {
+
+    readonly createTablemasterURL = "http://localhost:8080/createTablemaster/";
+    readonly createPlayerURL = "http://localhost:8080/createPlayer/";
+    readonly setSelectedCardURL = "http://localhost:8080/players/setselectedcard/";
+    readonly setPlayerModeURL = "http://localhost:8080/players/setplayermode/";
+    readonly getPlayerModeURL = "http://localhost:8080/players/getplayermode/";
+    readonly averageURL = "http://localhost:8080/average/";
+    readonly getPlayerURL = "http://localhost:8080/tables/getplayers/";
+
+    constructor(private httpClient: HttpClient) { }
+
+
+    createTablemaster(tablemasterName: string, tablename: string) {
+       return this.httpClient.get(this.createTablemasterURL + tablemasterName + "/" + tablename, {responseType: 'text'});
+    }
+
+    createPlayer(gamekey: string, playername: string) {
+        return this.httpClient.get<number>(this.createPlayerURL + playername + "/" + gamekey, {responseType: 'json'});
+    }
+
+    setSelectedCard(gamekey: string, playerid: number, selectedCard: string) {
+       return this.httpClient.get(this.setSelectedCardURL + gamekey + "/" + playerid + "/" + selectedCard);
+    }
+
+    setPlayerMode(gamekey: string, playerid: number, isPlaying: boolean) {
+       return this.httpClient.get(this.setPlayerModeURL + gamekey + "/" + playerid + "/" + isPlaying);
+    }
+    getPlayerMode(gamekey: string, playerid: number) {
+       return this.httpClient.get(this.getPlayerModeURL + gamekey + "/" + playerid + "/");
+    }
+    getAverage(gamekey: string) {
+       return this.httpClient.get<number>(this.averageURL + gamekey, {responseType: 'json'});
+    }
+
+    getPlayers(gamekey: string) {
+        return this.httpClient.get<Player[]>(this.getPlayerURL + gamekey, {responseType: 'json'});
+    }
+}
