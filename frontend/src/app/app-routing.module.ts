42f36a7b0c56f22051bcd5f59b1d8b2a442f3cdc
Jonas Schmid <schmid@puzzle.ch>
2021-11-30 16:38:54 +0100

    fix empty route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     {
         path: '',
         pathMatch: 'full',
-        /*redirectTo: 'onborarding'*/
+        redirectTo: 'onborarding'
     },
     {
         path: 'onborarding',
