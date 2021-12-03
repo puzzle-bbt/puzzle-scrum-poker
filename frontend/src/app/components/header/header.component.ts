61bbd521d32e339c2fb0e58fcc8e9b5e5015c3fd
Lias <kleisa@puzzle.ch>
2021-12-03 16:17:54 +0100

    Add info button and button copy game link and add spectator mode for player and tablemaster

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -10,6 +10,7 @@ import {PokerGameService} from "../../services/poker-game.service";
 export class HeaderComponent implements OnInit {
 
     isPlaying?: boolean = true;
+    isOnPlayground?: boolean;
 
     constructor(private cacheService: CacheService, private pokerGameService: PokerGameService) {
     }
@@ -19,13 +20,21 @@ export class HeaderComponent implements OnInit {
 
     public changeSpectator() {
         if (this.isPlaying) {
-            this.pokerGameService.setPlayerMode(this.cacheService.gamekey!, this.cacheService.id!, false);
+            this.pokerGameService.setPlayerMode(this.cacheService.gamekey!, this.cacheService.id!, false).subscribe();
             this.isPlaying = false;
         }
         else {
-            this.pokerGameService.setPlayerMode(this.cacheService.gamekey!, this.cacheService.id!, true);
+            this.pokerGameService.setPlayerMode(this.cacheService.gamekey!, this.cacheService.id!, true).subscribe();
             this.isPlaying = true;
         }
     }
 
+    public isCurrentlyOnPlayground() {
+        let url = window.location.toString();
+        if (url.includes("playground")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- frontend/src/app/components/playerlist/playerlist.component.ts
+++ frontend/src/app/components/playerlist/playerlist.component.ts
@@ -65,4 +65,18 @@ export class PlayerListComponent implements OnInit{
             }
         );
     }
+
+    public copyLink() {
+        const selBox = document.createElement('textarea');
+        selBox.style.position = 'fixed';
+        selBox.style.left = '0';
+        selBox.style.top = '0';
+        selBox.style.opacity = '0';
+        selBox.value = "localhost:4200/onboarding/" + this.gamekey;
+        document.body.appendChild(selBox);
+        selBox.focus();
+        selBox.select();
+        document.execCommand('copy');
+        document.body.removeChild(selBox);
+    }
 }
