6a4d35287deb42d6267726efa93c6303603c0420
Lias <kleisa@puzzle.ch>
2021-12-03 11:45:37 +0100

    Turn off start stop button for player but not for tablemaster

--- frontend/src/app/components/playground/playground.component.ts
+++ frontend/src/app/components/playground/playground.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CacheService} from "../../services/cache.service";
 
 @Component({
   selector: 'app-playground',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaygroundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cacheService: CacheService) { }
 
   ngOnInit(): void {
+      console.log(this.cacheService.isTablemaster);
+      if (!this.cacheService.isTablemaster) {
+          const element = <HTMLElement> document.getElementsByClassName('middleButton')[0];
+          element.style.display = 'none';
+      }
   }
 
 }
