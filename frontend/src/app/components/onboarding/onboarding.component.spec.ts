4162f966776c351b7a8f259d0e38035993c2d227
Lias <kleisa@puzzle.ch>
2021-12-21 15:49:34 +0100

    Begin with testing onboardingComponent

--- frontend/src/app/components/onboarding/onboarding.component.spec.ts
+++ frontend/src/app/components/onboarding/onboarding.component.spec.ts
@@ -1,14 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {OnboardingComponent} from './onboarding.component';
+import {ActivatedRoute, ActivatedRouteSnapshot, convertToParamMap} from "@angular/router";
+import {PokerGameService} from "../../services/poker-game.service";
+import {RouterTestingModule} from "@angular/router/testing";
+import createSpyObj = jasmine.createSpyObj;
 
-import { OnboardingComponent } from './onboarding.component';
 
 describe('OnboardingComponent', () => {
   let component: OnboardingComponent;
   let fixture: ComponentFixture<OnboardingComponent>;
+  let pokerGameServiceSpy: jasmine.SpyObj<PokerGameService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [OnboardingComponent]
+  beforeEach(() => {
+    pokerGameServiceSpy = createSpyObj('PokerGameService', ['setGameKey', 'setAsTableMaster', 'createTablemaster', 'createPlayer']);
+    pokerGameServiceSpy.setGameKey.and.callThrough();
+    pokerGameServiceSpy.setAsTableMaster.and.callThrough();
+    pokerGameServiceSpy.createTablemaster.and.callThrough();
+    pokerGameServiceSpy.createPlayer.and.callThrough();
+
+    TestBed.configureTestingModule({
+      declarations: [OnboardingComponent],
+      imports: [RouterTestingModule],
+      providers: [
+        {provide: PokerGameService, useValue: pokerGameServiceSpy},
+        {provide: ActivatedRoute, useValue: {snapshot: {paramMap: convertToParamMap({gamekey: '11'})}}},
+      ]
     })
       .compileComponents();
   });
@@ -16,7 +32,6 @@ describe('OnboardingComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(OnboardingComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
