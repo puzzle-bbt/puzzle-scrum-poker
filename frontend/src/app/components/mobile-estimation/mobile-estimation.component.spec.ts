51519554df2bdefc6651354644b9a15748c3479c
Niklas Jäggi <jaeggi@puzzle.ch>
2021-12-22 13:31:37 +0100

    mobile-estimation unit test working again

--- frontend/src/app/components/mobile-estimation/mobile-estimation.component.spec.ts
+++ frontend/src/app/components/mobile-estimation/mobile-estimation.component.spec.ts
@@ -1,22 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MobileEstimationComponent } from './mobile-estimation.component';
+import {PokerGameService} from "../../services/poker-game.service";
+import createSpyObj = jasmine.createSpyObj;
+import {BackendMessengerService} from "../../services/backend-messenger.service";
+import {BehaviorSubject} from "rxjs";
+import {Game} from "../../models/model";
 
 describe('MobileEstinationComponent', () => {
   let component: MobileEstimationComponent;
   let fixture: ComponentFixture<MobileEstimationComponent>;
+  let pokerGameServiceSpy: jasmine.SpyObj<PokerGameService>;
+  let messengerServiceSpy: jasmine.SpyObj<BackendMessengerService>;
+  let subjectMock: BehaviorSubject<Game>;
 
   beforeEach(async () => {
+    subjectMock = new BehaviorSubject<Game>({
+      gameKey: "0",
+      iAmTableMaster: false,
+      isGameRunning: false,
+    }as Game)
+    pokerGameServiceSpy = createSpyObj('PokerGameService', ['setSelectedCard', 'getRoundName', 'getAverage'], {'game$' : subjectMock});
+    messengerServiceSpy = createSpyObj('BackendMessengerService', ['subscribe']);
+
     await TestBed.configureTestingModule({
-      declarations: [MobileEstimationComponent]
+      declarations: [MobileEstimationComponent],
+      providers: [
+        {provide: PokerGameService, useValue: pokerGameServiceSpy},
+        {provide: BackendMessengerService, useValue: messengerServiceSpy}
+      ]
     })
-      .compileComponents();
+        .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MobileEstimationComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
