a39528abd08329282e361f195332360118da7299
Niklas Jäggi <jaeggi@puzzle.ch>
2021-12-27 12:52:06 +0100

    playerlist-component, playground and error component tests running again

--- frontend/src/app/components/error/error.component.spec.ts
+++ frontend/src/app/components/error/error.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import createSpyObj = jasmine.createSpyObj;
 
 import { ErrorComponent } from './error.component';
+import {PokerGameService} from "../../services/poker-game.service";
+import {BehaviorSubject} from "rxjs";
+import {UserError} from "../../models/model";
 
 describe('ErrorComponent', () => {
   let component: ErrorComponent;
   let fixture: ComponentFixture<ErrorComponent>;
+  let pokerGameServiceSpy: jasmine.SpyObj<PokerGameService>;
+  let subjectMock: BehaviorSubject<UserError>;
 
   beforeEach(async () => {
+    subjectMock = new BehaviorSubject<UserError>({}as UserError)
+    pokerGameServiceSpy = createSpyObj('PokerGameService', [], {'error$' : subjectMock});
+
     await TestBed.configureTestingModule({
-      declarations: [ErrorComponent]
+      declarations: [ErrorComponent],
+      providers: [
+        {provide: PokerGameService, useValue: pokerGameServiceSpy},
+      ]
     })
       .compileComponents();
   });
--- frontend/src/app/components/playground/playground.component.spec.ts
+++ frontend/src/app/components/playground/playground.component.spec.ts
@@ -1,14 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import createSpyObj = jasmine.createSpyObj;
 
 import { PlaygroundComponent } from './playground.component';
+import {PokerGameService} from "../../services/poker-game.service";
+import {BehaviorSubject} from "rxjs";
+import {Game} from "../../models/model";
 
 describe('PlaygroundComponent', () => {
   let component: PlaygroundComponent;
   let fixture: ComponentFixture<PlaygroundComponent>;
+  let pokerGameServiceSpy: jasmine.SpyObj<PokerGameService>;
+  let subjectMock: BehaviorSubject<Game>;
 
   beforeEach(async () => {
+    subjectMock = new BehaviorSubject<Game>({
+      gameKey: "0",
+      iAmTableMaster: false,
+      isGameRunning: false,
+    }as Game)
+    pokerGameServiceSpy = createSpyObj('PokerGameService', [], {'game$' : subjectMock});
+
     await TestBed.configureTestingModule({
-      declarations: [PlaygroundComponent]
+      declarations: [PlaygroundComponent],
+      providers: [
+        {provide: PokerGameService, useValue: pokerGameServiceSpy},
+      ]
     })
       .compileComponents();
   });
