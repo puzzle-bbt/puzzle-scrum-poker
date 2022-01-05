352a5d9692b2ee0469b366a150bdd8611ed843e2
Hannes Pfammatter <hannepfa016@gmail.com>
2022-01-05 14:42:09 +0100

    fix existing tests in onboarding component

--- frontend/src/app/components/onboarding/onboarding.component.spec.ts
+++ frontend/src/app/components/onboarding/onboarding.component.spec.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute, ActivatedRouteSnapshot, convertToParamMap } from '@angu
 import { PokerGameService } from '../../services/poker-game.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Game, Player, UserError } from '../../models/model';
-import createSpyObj = jasmine.createSpyObj;
 import {BehaviorSubject, Observable, of} from 'rxjs';
 
 const GAME_INIT_VALUE: Game = {
@@ -22,29 +21,44 @@ const ERROR_INIT_VALUE: UserError = {
   message: ''
 }
 
+class PokerGameServiceStub implements Partial<PokerGameService> {
+  game$ = new BehaviorSubject<Game>(GAME_INIT_VALUE);
+  players$ = new BehaviorSubject<Player[]>([]);
+  error$ = new BehaviorSubject<UserError>(ERROR_INIT_VALUE);
+
+  setGameKey(gameKey: string) {
+    this.game$.next({...this.game$.value, gameKey: gameKey});
+  }
+  setAsTableMaster() {
+    this.game$.next({...this.game$.value, iAmTableMaster: true});
+  }
+  createTablemaster(tablemasterName: string): Observable<boolean> {
+    return of(true);
+  }
+  public createPlayer(playerName: string): Observable<boolean> {
+    return of(true);
+  }
+}
+
 describe('OnboardingComponent', () => {
   let component: OnboardingComponent;
   let fixture: ComponentFixture<OnboardingComponent>;
-  let pokerGameServiceSpy: jasmine.SpyObj<PokerGameService>;
+  let pokerGameServiceStub: PokerGameServiceStub;
+  let setGameKeySpy: jasmine.Spy<(gameKey: string) => void>;
+  let setAsTableMasterSpy: jasmine.Spy<() => void>;
 
   beforeEach(() => {
-    pokerGameServiceSpy = createSpyObj('PokerGameService',
-      ['setGameKey', 'setAsTableMaster', 'createTablemaster', 'createPlayer'],
-      {
-        game$: new BehaviorSubject<Game>(GAME_INIT_VALUE),
-        players$: new BehaviorSubject<Player[]>([]),
-        error$: new BehaviorSubject<UserError>(ERROR_INIT_VALUE)
-      });
-    pokerGameServiceSpy.setGameKey.and.callThrough();
-    pokerGameServiceSpy.createTablemaster.and.callThrough();
-    pokerGameServiceSpy.createPlayer.and.callThrough();
-    pokerGameServiceSpy.createPlayer.and.returnValue(of(true));
+    pokerGameServiceStub = new PokerGameServiceStub();
+    setGameKeySpy = spyOn(pokerGameServiceStub, "setGameKey").and.callThrough();
+    setAsTableMasterSpy = spyOn(pokerGameServiceStub, "setAsTableMaster").and.callThrough();
+    spyOn(pokerGameServiceStub, "createTablemaster").and.callThrough();
+    spyOn(pokerGameServiceStub, "createPlayer").and.callThrough();
 
     TestBed.configureTestingModule({
       declarations: [OnboardingComponent],
       imports: [RouterTestingModule],
       providers: [
-        {provide: PokerGameService, useValue: pokerGameServiceSpy},
+        {provide: PokerGameService, useValue: pokerGameServiceStub},
         {provide: ActivatedRoute, useValue: {snapshot: {paramMap: convertToParamMap({gamekey: '11'})}}},
       ]
     })
@@ -54,6 +68,7 @@ describe('OnboardingComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(OnboardingComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -61,41 +76,41 @@ describe('OnboardingComponent', () => {
   });
 
   it('should onboarding for master', () => {
+    setGameKeySpy.calls.reset();
+    setAsTableMasterSpy.calls.reset();
     var activatedRoute = TestBed.inject(ActivatedRoute);
     activatedRoute.snapshot = {paramMap: convertToParamMap({})} as ActivatedRouteSnapshot;
     component.ngOnInit();
-    expect(pokerGameServiceSpy.setGameKey).not.toHaveBeenCalled();
-    expect(pokerGameServiceSpy.setAsTableMaster).toHaveBeenCalled();
+    expect(pokerGameServiceStub.setGameKey).not.toHaveBeenCalled();
+    expect(pokerGameServiceStub.setAsTableMaster).toHaveBeenCalled();
   });
 
   it('should onboarding for player', () => {
-    component.ngOnInit();
-    expect(pokerGameServiceSpy.setGameKey).toHaveBeenCalledOnceWith('11');
+    expect(pokerGameServiceStub.setGameKey).toHaveBeenCalledOnceWith('11');
+    expect(pokerGameServiceStub.setAsTableMaster).not.toHaveBeenCalled();
   });
 
   it('call createTablemaster', () => {
+    spyOn(component, "createTablemaster").and.callThrough();
     var activatedRoute = TestBed.inject(ActivatedRoute);
     activatedRoute.snapshot = {paramMap: convertToParamMap({})} as ActivatedRouteSnapshot;
     component.ngOnInit();
-    pokerGameServiceSpy.setAsTableMaster();
     component.create('Test');
     expect(component.createTablemaster).toHaveBeenCalledWith("Test");
-    expect(pokerGameServiceSpy.createPlayer).not.toHaveBeenCalled();
+    expect(pokerGameServiceStub.createPlayer).not.toHaveBeenCalled();
   });
 
   it('call createPlayer', () => {
-    var activatedRoute = TestBed.inject(ActivatedRoute);
-    activatedRoute.snapshot = {paramMap: convertToParamMap({})} as ActivatedRouteSnapshot;
-    component.ngOnInit();
+    spyOn(component, "createPlayer").and.callThrough();
     component.create('Test');
     expect(component.createPlayer).toHaveBeenCalledOnceWith('Test');
-    expect(pokerGameServiceSpy.createTablemaster).not.toHaveBeenCalled();
+    expect(pokerGameServiceStub.createTablemaster).not.toHaveBeenCalled();
   });
 
   it('createPlayer', () => {
     component.ngOnInit();
     component.create('Test');
-    expect(pokerGameServiceSpy.createPlayer).toHaveBeenCalledOnceWith('Test');
+    expect(pokerGameServiceStub.createPlayer).toHaveBeenCalledOnceWith('Test');
   });
 
 });
