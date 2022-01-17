31c0db48023bcc176c1dbeb635e2629757faf1cd
Jonas Schmid <schmid@puzzle.ch>
2022-01-17 13:45:50 +0100

    refactoring kickplayer.component.spec.ts and doesn't mock the local method 'navigateToHome'

--- frontend/src/app/components/kickplayer/kickplayer.component.spec.ts
+++ frontend/src/app/components/kickplayer/kickplayer.component.spec.ts
@@ -1,13 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KickplayerComponent } from './kickplayer.component';
-import {ActivatedRoute, convertToParamMap, Router} from "@angular/router";
-import {DebugElement} from "@angular/core";
-import {By} from "@angular/platform-browser";
-import {RouterTestingModule} from "@angular/router/testing";
-import {PokerGameService} from "../../services/poker-game.service";
-import {BehaviorSubject, of} from "rxjs";
-import {Game, Player} from "../../models/model";
+import { Router } from '@angular/router';
 import createSpyObj = jasmine.createSpyObj;
 
 describe('KickplayerComponent', () => {
@@ -20,17 +14,14 @@ describe('KickplayerComponent', () => {
 
   routerSpy.navigate.and.callThrough();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ KickplayerComponent ],
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [KickplayerComponent],
       providers: [
         {provide: Router, useValue: routerSpy},
       ]
-    })
-    .compileComponents();
-  });
+    });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(KickplayerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -41,22 +32,16 @@ describe('KickplayerComponent', () => {
   });
 
   it('should have button text', () => {
-    const kickPlayerComponent: DebugElement = fixture.debugElement;
-    const okButton = kickPlayerComponent.query(By.css('button'));
-    const okButtonHtml: HTMLElement = okButton.nativeElement;
-    expect(okButtonHtml.textContent).toEqual('OK');
+    const okButton = fixture.nativeElement.querySelector('button');
+    expect(okButton.textContent).toEqual('OK');
   })
 
-  it('should clickButton',  () => {
-    const kickPlayerComponent: DebugElement = fixture.debugElement;
-    const okButton = kickPlayerComponent.query(By.css('button'));
-    const okButtonHtml: HTMLElement = okButton.nativeElement;
-
-    let navigateSpy = spyOn(component, "navigateToHome");
-    okButtonHtml.click();
+  it('should clickButton', () => {
+    const okButton = fixture.nativeElement.querySelector('button');
+    okButton.click();
     fixture.detectChanges();
-    expect(navigateSpy).toHaveBeenCalledTimes(1);
-
+    expect(routerSpy.navigate).toHaveBeenCalledTimes(1);
+    expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['/onboarding/']);
   });
 
 });
