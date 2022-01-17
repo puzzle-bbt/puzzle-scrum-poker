1310d855ebb68ab3f7f194d771910925b3badf99
Jonas Schmid <schmid@puzzle.ch>
2022-01-17 11:20:33 +0100

    fix 'should copy link' and 'isOnDesktop' unit tests

--- frontend/src/app/components/playerlist/playerlist.component.spec.ts
+++ frontend/src/app/components/playerlist/playerlist.component.spec.ts
@@ -1,12 +1,10 @@
-import {ComponentFixture, TestBed, tick} from '@angular/core/testing';
-import {PlayerListComponent} from './playerlist.component';
-import {PokerGameService} from "../../services/poker-game.service";
-import {BehaviorSubject, Observable, of} from "rxjs";
-import {Game, Player} from "../../models/model";
-import {BackendMessengerService} from "../../services/backend-messenger.service";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PlayerListComponent } from './playerlist.component';
+import { PokerGameService } from '../../services/poker-game.service';
+import { BehaviorSubject, of } from 'rxjs';
+import { Game, Player } from '../../models/model';
+import { BackendMessengerService } from '../../services/backend-messenger.service';
 import createSpyObj = jasmine.createSpyObj;
-import {DebugElement} from "@angular/core";
-import {By} from "@angular/platform-browser";
 
 const GAME_INIT_VALUE: Game = {
   gameKey: '0',
@@ -20,7 +18,7 @@ const GAME_INIT_VALUE: Game = {
 
 const PLAYER_INIT_VALUE: Player = {
   id: 1,
-  name: "Player",
+  name: 'Player',
   playing: true,
   selectedCard: undefined
 };
@@ -30,41 +28,40 @@ describe('PlayerListComponent', () => {
   let fixture: ComponentFixture<PlayerListComponent>;
   let pokerGameServiceSpy: jasmine.SpyObj<PokerGameService>;
   let messengerServiceSpy: jasmine.SpyObj<BackendMessengerService>;
+  let copyButton: HTMLElement;
 
-  beforeEach( () => {
-     pokerGameServiceSpy = createSpyObj('PokerGameService',
-       ['getPlayers', 'kickplayer'],
-       {
-         game$: new BehaviorSubject<Game>(GAME_INIT_VALUE),
-         players$: new BehaviorSubject<Player>(PLAYER_INIT_VALUE)
-       });
+  beforeEach(() => {
+    pokerGameServiceSpy = createSpyObj('PokerGameService',
+      ['getPlayers', 'kickplayer'],
+      {
+        game$: new BehaviorSubject<Game>({
+          ...GAME_INIT_VALUE,
+          iAmTableMaster: true
+        }),
+        players$: new BehaviorSubject<Player[]>([PLAYER_INIT_VALUE])
+      });
     messengerServiceSpy = createSpyObj('BackendMessengerService', ['subscribe']);
 
     pokerGameServiceSpy.getPlayers.and.returnValue(of(PLAYER_INIT_VALUE));
     pokerGameServiceSpy.kickplayer.and.returnValue(of());
 
-  TestBed.configureTestingModule({
-    declarations: [PlayerListComponent],
-    providers: [
-      {provide: PokerGameService, useValue: pokerGameServiceSpy},
-      {provide: BackendMessengerService, useValue: messengerServiceSpy}
-    ]
-
-  })
-    .compileComponents();
-});
-
-beforeEach(() => {
-  fixture = TestBed.createComponent(PlayerListComponent);
-  component = fixture.componentInstance;
-});
+    TestBed.configureTestingModule({
+      declarations: [PlayerListComponent],
+      providers: [
+        {provide: PokerGameService, useValue: pokerGameServiceSpy},
+        {provide: BackendMessengerService, useValue: messengerServiceSpy}
+      ]
+    });
+    fixture = TestBed.createComponent(PlayerListComponent);
+    component = fixture.componentInstance;
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it("should ngOnit", () => {
-    let spyRefresh = spyOn(component, "refresh");
+  it('should ngOnit', () => {
+    let spyRefresh = spyOn(component, 'refresh');
 
     component.ngOnInit();
     expect(spyRefresh).toHaveBeenCalledTimes(1);
@@ -78,29 +75,40 @@ beforeEach(() => {
 
   it('call kickPlayer', () => {
     component.kickPlayer(1);
-    expect(pokerGameServiceSpy.kickplayer).toHaveBeenCalledWith("0", 1);
+    expect(pokerGameServiceSpy.kickplayer).toHaveBeenCalledWith('0', 1);
   });
 
-  it('should copy link', () => {
-    component.game$.next({
-      ...GAME_INIT_VALUE,
-      iAmTableMaster: true
-    });
-    const playerListComponent: DebugElement = fixture.debugElement;
-    const copyButton = playerListComponent.query(By.css('button'));
-    const copyButtonHtml: HTMLElement = copyButton.nativeElement;
-    expect(copyButtonHtml.textContent).toEqual('Einladungslink kopieren');
+  it('should call copyLink function', () => {
+    let spyCopyLink = spyOn(component, 'copyLink');
+    fixture.detectChanges();
+    copyButton = fixture.nativeElement.querySelector('button');
 
+    expect(copyButton.textContent).toContain('Einladungslink kopieren');
+    copyButton.click();
+    expect(spyCopyLink).toHaveBeenCalledTimes(1);
   });
 
-  it('isOnDesktop', () => {
-    var isOnDesktop = component.isOnDesktop();
-    if (window.innerWidth > 768) {
-      expect(isOnDesktop).toBeTrue();
-    } else {
-      expect(isOnDesktop).toBeFalse();
+  it('should execute copyLink function without error', (done) => {
+    try {
+      component.copyLink();
+      done();
+    } catch (error) {
+      fail('a error is occurred: ' + error);
     }
   });
 
+  it('should by isOnDesktop true', () => {
+    spyOnProperty(window, 'innerWidth').and.returnValue(769);
+    window.dispatchEvent(new Event('resize'));
+    var isOnDesktop = component.isOnDesktop();
+    expect(isOnDesktop).toBeTrue();
+  });
+
+  it('should by isOnDesktop false', () => {
+    spyOnProperty(window, 'innerWidth').and.returnValue(767);
+    window.dispatchEvent(new Event('resize'));
+    var isOnDesktop = component.isOnDesktop();
+    expect(isOnDesktop).toBeFalse();
+  });
 
 });
