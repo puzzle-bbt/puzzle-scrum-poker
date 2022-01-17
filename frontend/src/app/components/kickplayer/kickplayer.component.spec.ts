05adc68d56e52205b3ec82a5fb6504702ca92743
Lias <kleisa@puzzle.ch>
2022-01-17 12:07:45 +0100

    Add unti test for kickPlayer and add router in test

--- frontend/src/app/components/kickplayer/kickplayer.component.spec.ts
+++ frontend/src/app/components/kickplayer/kickplayer.component.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KickplayerComponent } from './kickplayer.component';
+import {ActivatedRoute, convertToParamMap, Router} from "@angular/router";
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
+import {RouterTestingModule} from "@angular/router/testing";
+import {PokerGameService} from "../../services/poker-game.service";
+import {BehaviorSubject, of} from "rxjs";
+import {Game, Player} from "../../models/model";
+import createSpyObj = jasmine.createSpyObj;
 
 describe('KickplayerComponent', () => {
   let component: KickplayerComponent;
   let fixture: ComponentFixture<KickplayerComponent>;
+  let routerSpy: jasmine.SpyObj<Router>;
+
+  routerSpy = createSpyObj('Router',
+    ['navigate']);
+
+  routerSpy.navigate.and.callThrough();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ KickplayerComponent ]
+      declarations: [ KickplayerComponent ],
+      providers: [
+        {provide: Router, useValue: routerSpy},
+      ]
     })
     .compileComponents();
   });
@@ -22,4 +39,24 @@ describe('KickplayerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have button text', () => {
+    const kickPlayerComponent: DebugElement = fixture.debugElement;
+    const okButton = kickPlayerComponent.query(By.css('button'));
+    const okButtonHtml: HTMLElement = okButton.nativeElement;
+    expect(okButtonHtml.textContent).toEqual('OK');
+  })
+
+  it('should clickButton',  () => {
+    const kickPlayerComponent: DebugElement = fixture.debugElement;
+    const okButton = kickPlayerComponent.query(By.css('button'));
+    const okButtonHtml: HTMLElement = okButton.nativeElement;
+
+    let navigateSpy = spyOn(component, "navigateToHome");
+    okButtonHtml.click();
+    fixture.detectChanges();
+    expect(navigateSpy).toHaveBeenCalledTimes(1);
+
+  });
+
 });
