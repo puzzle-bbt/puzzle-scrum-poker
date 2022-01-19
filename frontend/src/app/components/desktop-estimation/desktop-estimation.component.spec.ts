e5e5a03bbc08e52dbc7ffdf70565d17ac362b67a
Niklas Jäggi <jaeggi@puzzle.ch>
2022-01-19 10:24:57 +0100

    removed redundant code

--- frontend/src/app/components/desktop-estimation/desktop-estimation.component.spec.ts
+++ frontend/src/app/components/desktop-estimation/desktop-estimation.component.spec.ts
@@ -103,64 +103,7 @@ describe('DesktopEstinationComponent', () => {
     let frontCards = document.getElementById('cardContainerFront');
     let backCards = document.getElementById('cardContainerBack');
 
-    playerMock.value.playing = false;
-    component.refresh();
-    frontCards!.classList.contains('hidden');
-    backCards!.classList.contains('hidden');
-
-    playerMock.value.playing = true;
-    component.refresh();
-    frontCards!.classList.contains('visible');
-    backCards!.classList.contains('hidden');
-
-    pokerGameServiceSpy.game$.value.isGameRunning = false;
-    component.refresh();
-    frontCards!.classList.contains('hidden');
-    backCards!.classList.contains('visible');
-  });
-
-  it('should getAverage', () => {
-    component.getAverage();
-    expect(pokerGameServiceSpy.getAverage).toHaveBeenCalledTimes(1);
-  });
-
-  it('should ngOnIt', () => {
-    let spyRefresh = spyOn(component, 'refresh');
-    let spyAddCards = spyOn(component, 'addCards');
-
-    component.ngOnInit();
-    expect(spyRefresh).toHaveBeenCalledTimes(1);
-    expect(spyAddCards).toHaveBeenCalledTimes(1);
-  });
-
-  it('should addCards', () => {
-    component.addCards();
-    expect(pokerGameServiceSpy.getCardSvg).toHaveBeenCalledTimes(1);
-  });
-
-  it('should turnCards', () => {
-    let spyResetCards = spyOn(component, 'resetCards');
-    let frontCards = document.getElementById('cardContainerFront');
-    let backCards = document.getElementById('cardContainerBack');
-
     pokerGameServiceSpy.game$.value.isGameRunning = true;
-    component.turnCards();
-    expect(backCards!.classList.contains('visible'));
-    expect(frontCards!.classList.contains('hidden'));
-
-
-    pokerGameServiceSpy.game$.value.isGameRunning = false;
-    component.turnCards();
-    expect(spyResetCards).toHaveBeenCalledTimes(1);
-    expect(backCards!.classList.contains('hidden'));
-    expect(frontCards!.classList.contains('visible'));
-  });
-
-
-  it('should refresh', () => {
-    let frontCards = document.getElementById('cardContainerFront');
-    let backCards = document.getElementById('cardContainerBack');
-
     playerMock.value.playing = false;
     component.refresh();
     frontCards!.classList.contains('hidden');
